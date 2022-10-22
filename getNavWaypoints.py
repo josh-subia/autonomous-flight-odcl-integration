ba0362d566af47306bed7f3ac85fb585c94c2896
josh-subia <jsubia22@hawaii.edu>
2022-10-22 01:43:47 -1000

    getNavWaypoints
    Gets waypoints data as user input and stores in an array

---
+++ getNavWaypoints.py
@@ -0,0 +1,26 @@
+#create empty latitude list
+lat = [];
+#create empty longitude list
+long = [];
+#create empty altitude list
+alt = [];
+
+#get number of waypoints
+n = int(input("Enter number of waypoints: "))
+
+#iterate till all latitudes, longitudes, and altitudes are in their respective arrays
+for i in range(0, n):
+    #Get latitude,longitude and altitude
+    print("Enter waypoint", i, "data")
+    latIn = float(input("Enter latitude: "))
+    longIn = float(input("Enter longitude: "))
+    altIn = float(input("Enter altitude: "))
+    #append them to the array
+    lat.append(latIn)
+    long.append(longIn)
+    alt.append(altIn)
+
+for i in range (0, n):
+    print("Latitude", i, ":", lat[i])
+    print("Longitude", i, ":", long[i])
+    print("Altitude:", i, ":", alt[i])
