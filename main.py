0516a9847aa447898bd96cf3db7ea30cdb9125ff
josh-subia <jsubia22@hawaii.edu>
2022-10-23 14:17:10 -1000

    Added main.py updated getNavWPs

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-#This file is able to get waypoint data through user input and store it in an array
-#Work that still has to be done is to officially upload these waypoint data to Mission Planner
+#This is the main function that will run the autonomous-flight and odcl iteegrated script
+
+#imports
+import sys
+import math
+#import clr
+import time
+#import System
+#from System import Byte
+
+#Undecided if to be implemented yet: 
+# Get waypoint navigation waypoints through user input and navigate them
+
+
+# Takeoff Manual/Autonomous
 #create empty latitude list
 lat = [];
 #create empty longitude list
@@ -9,6 +22,8 @@ alt = [];
 
 #get number of waypoints
 n = int(input("Enter number of waypoints: "))
+#create empty list of n waypoints
+wp = [] * n;
 
 #iterate till all latitudes, longitudes, and altitudes are in their respective arrays
 for i in range(0, n):
@@ -23,6 +38,19 @@ for i in range(0, n):
     alt.append(altIn)
 
 for i in range (0, n):
+    #waypoints to search the air delivery area
+    wp[i] = Locationwp().Set(lat[i], long[i], alt[i], id) 
     print("Latitude", i, ":", lat[i])
     print("Longitude", i, ":", long[i])
-    print("Altitude:", i, ":", alt[i])
+    print("Altitude (in meters):", i, ":", alt[i])
+
+#Navigate the search area
+
+
+#If less than 5 identified targets, navigate the search area again
+
+
+#Payload delivery
+
+
+#Landing Manual/Autnomous
