95e1f752dd122426a2aa9300bd3f3e89c67c3ad6
josh-subia <jsubia22@hawaii.edu>
2022-10-24 19:17:10 -1000

    Update getNavWPs.py

--- getNavWPs.py
+++ getNavWPs.py
@@ -2,17 +2,17 @@
 import csv
 import sys
 import math
-#import clr
+import clr
 import time
-#import System
-#from System import Byte
+import System
+from System import Byte
 
 clr.AddReference("MissionPlanner")
-#import MissionPlanner
+import MissionPlanner
 clr.AddReference("MissionPlanner.Utilities") # includes the Utilities class
 from MissionPlanner.Utilities import Locationwp
 clr.AddReference("MAVLink") # includes the Utilities class
-#import MAVLink
+import MAVLink
 
 #create empty latitude list
 lat = []
@@ -29,14 +29,38 @@ with open('navWPs.csv') as csv_file:
     for row in csv_reader:
         rows.append(row)
 
+idmavcmd = MAVLink.MAV_CMD.WAYPOINT
+id = int(idmavcmd)
+#set RTH/RTL and flight termination point
+home = Locationwp().Set(38.315339, -76.548108, 0, id)
+to = Locationwp()
+Locationwp.id.SetValue(to, int(MAVLink.MAV_CMD.TAKEOFF))
+Locationwp.p1.SetValue(to, 15)
+Locationwp.alt.SetValue(to, 50)
+
+#number of waypoints
+wp = [] * len(rows[0])
+
 #store latitude, longitude, and altitude in an array
 for i in range(0, len(rows[0])):
     lat.append(rows[0][i])
     long.append(rows[1][i])
     alt.append(rows[2][i])
+    wp[i] = Locationwp().Set(lat[i], long[i], alt[i], id)
     #print("Latitude", i, ":", lat[i])
     #print("Longitude", i, ":", long[i])
     #print("Altitude", i, "(in meters):", alt[i])
 
-wp = [] * len(rows[0])
-print(len(rows[0]))
+print ("set wp total")
+#Total WP is len(rows[0]) + 1 to account for Home
+MAV.setWPTotal(len(rows[0]) + 1
+print ("upload to")
+MAV.setWP(to,0,MAVLink.MAV_FRAME.GLOBAL_RELATIVE_ALT);)
+
+#for loop to upload waypoints
+for i in range (1, n):
+    print ("upload wp", i)
+    MAV.setWP(wp[i], i, MAVLink.MAV_FRAME.GLOBAL_RELATIVE_ALT);
+
+print ("final ack")
+MAV.setWPACK();
