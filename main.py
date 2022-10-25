5bbf7e895182ca6c0eac981c704ae943d5bccb7c
josh-subia <jsubia22@hawaii.edu>
2022-10-24 19:08:49 -1000

    Update main.py

--- main.py
+++ main.py
@@ -13,36 +13,10 @@ import time
 
 
 # Takeoff Manual/Autonomous
-#create empty latitude list
-lat = [];
-#create empty longitude list
-long = [];
-#create empty altitude list
-alt = [];
 
-#get number of waypoints
-n = int(input("Enter number of waypoints: "))
-#create empty list of n waypoints
-wp = [] * n;
 
-#iterate till all latitudes, longitudes, and altitudes are in their respective arrays
-for i in range(0, n):
-    #Get latitude,longitude and altitude
-    print("Enter waypoint", i, "data")
-    latIn = float(input("Enter latitude: "))
-    longIn = float(input("Enter longitude: "))
-    altIn = float(input("Enter altitude: "))
-    #append them to the array
-    lat.append(latIn)
-    long.append(longIn)
-    alt.append(altIn)
+#conduct waypoint navigation portion of mission demonstration
 
-for i in range (0, n):
-    #waypoints to search the air delivery area
-    wp[i] = Locationwp().Set(lat[i], long[i], alt[i], id) 
-    print("Latitude", i, ":", lat[i])
-    print("Longitude", i, ":", long[i])
-    print("Altitude (in meters):", i, ":", alt[i])
 
 #Navigate the search area
 
