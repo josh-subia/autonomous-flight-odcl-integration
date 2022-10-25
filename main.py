241a1edc04c3511cbf4bc545c77cc636970092dd
josh-subia <jsubia22@hawaii.edu>
2022-10-24 19:40:42 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 #This is the main function that will run the autonomous-flight and odcl iteegrated script
 
 #imports
+import csv
 import sys
 import math
-#import clr
+import clr
 import time
-#import System
-#from System import Byte
+import System
+from System import Byte
+
+clr.AddReference("MissionPlanner")
+import MissionPlanner
+clr.AddReference("MissionPlanner.Utilities") # includes the Utilities class
+from MissionPlanner.Utilities import Locationwp
+clr.AddReference("MAVLink") # includes the Utilities class
+import MAVLink
+
+#import python files
+from getNavWPs import *
 
 #Undecided if to be implemented yet: 
 # Get waypoint navigation waypoints through user input and navigate them
@@ -16,7 +27,7 @@ import time
 
 
 #conduct waypoint navigation portion of mission demonstration
-
+getNavWPs()
 
 #Navigate the search area
 
