f8fc5ec5dce520d0a7ff3a061134abd88657bb60
josh-subia <jsubia22@hawaii.edu>
2022-12-03 20:57:26 -1000

    deleted testTime.py

--- payloadDelivery.py
+++ payloadDelivery.py
@@ -16,13 +16,17 @@ import MAVLink
 idmavcmd = MAVLink.MAV_CMD.WAYPOINT
 id = int(idmavcmd)
 
+test = 1
+while test < 5:
+    print("Test: ", test)
+    test += 1
+    time.sleep(1)
+
 target_lat = [38.3143699, 38.3143152, 38.3143152, 38.3143742, 38.3144794]
 target_long = [-76.5450177, -76.5447387, -76.5443256,-76.5441540, -76.5447119]
 target_id = []
 #altitude is 26 meters, approximately 85 feet high
 alt = 26
-#num_wps
-
 #number of waypoints
 wp = [0] * len(target_lat)
 
